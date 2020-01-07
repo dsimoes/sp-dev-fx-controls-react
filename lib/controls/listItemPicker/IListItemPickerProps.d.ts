ebf4bf481e24ca562ce4fdea00072514c72c0aa6
Daniel Simões <dsimoes@live.com>
2020-01-07 15:37:45 +0000

    Added compiled lib folder

---
+++ lib/controls/listItemPicker/IListItemPickerProps.d.ts
@@ -0,0 +1,17 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+import { ExtensionContext } from "@microsoft/sp-extension-base";
+export interface IListItemPickerProps {
+    columnInternalName: string;
+    keyColumnInternalName?: string;
+    context: WebPartContext | ExtensionContext;
+    listId: string;
+    itemLimit: number;
+    filter?: string;
+    className?: string;
+    webUrl?: string;
+    defaultSelectedItems?: any[];
+    disabled?: boolean;
+    suggestionsHeaderText?: string;
+    noResultsFoundText?: string;
+    onSelectedItem: (item: any) => void;
+}
